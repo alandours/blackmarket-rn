8bf133ba18fd8ddba99bd6e5044de0043dde181c
Alan Dours <alan.dours@rootstrap.com>
2023-04-11 18:17:07 -0300

    Add en locale

---
+++ src/locale/en.ts
@@ -0,0 +1,29 @@
+export const en = {
+  a11y: {
+    logo: 'Black Market logo',
+  },
+  auth: {
+    login: { title: 'Already have an account?', cta: 'Log in' },
+    signup: {
+      title: 'Don’t have an account?',
+      cta: 'Sign up',
+      policy: 'By signing up, you accept the Data Policy and the Cookies Policy.',
+    },
+    forgotPassword: 'I forgot my password',
+    error: 'Sorry! Your email or password are incorrect.',
+    fields: {
+      email: {
+        label: 'Email',
+        placeholder: 'Type your email',
+      },
+      password: {
+        label: 'Password',
+        placeholder: 'Type your password',
+      },
+      fullName: {
+        label: 'Full name',
+        placeholder: 'Type your full name',
+      },
+    },
+  },
+};
